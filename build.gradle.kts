c7dbba4905d4aa978b7f54069d86a03f814ffff8
root <root@mytest.server>
2026-02-22 20:41:15 +0500

    Fix build errors: remove deprecated android.enableUnitTestBinaryResources and remove duplicate repositories

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,6 @@ buildscript {
 }
 
 allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-    
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions {
             jvmTarget = "17"
