682339499fc569331bfcc24472b788ae475db2c5
root <root@mytest.server>
2026-02-23 08:16:09 +0500

    Fix VouchersTab: add missing Box import and collectAsState

--- app/src/main/kotlin/com/betting/manager/ui/tabs/VouchersTab.kt
+++ app/src/main/kotlin/com/betting/manager/ui/tabs/VouchersTab.kt
@@ -1,6 +1,7 @@
 package com.betting.manager.ui.tabs
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -27,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
