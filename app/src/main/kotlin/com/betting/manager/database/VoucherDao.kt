0c56ea8fbc34dac9b52da7fc39ac11422a050cc0
root <root@mytest.server>
2026-02-23 07:26:39 +0500

    Add missing @Embedded import for Room relationship

--- app/src/main/kotlin/com/betting/manager/database/VoucherDao.kt
+++ app/src/main/kotlin/com/betting/manager/database/VoucherDao.kt
@@ -1,6 +1,7 @@
 package com.betting.manager.database
 
 import androidx.room.Dao
+import androidx.room.Embedded
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
